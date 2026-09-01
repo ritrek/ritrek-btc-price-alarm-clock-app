46b6f967d2c2b96620b72a8b88fc16a77a1addf2
Oren <orenz0@protonmail.com>
2026-09-02 01:03:57 +0300

    fix image-size replacer

--- overrides/image-size/index.js
+++ overrides/image-size/index.js
@@ -1,25 +1,44 @@
 'use strict';
 
+const fs = require('fs');
 const loaded = require('image-size-next');
-const imageSize =
+const imageSizeFn =
   typeof loaded === 'function'
     ? loaded
     : loaded.imageSize || loaded.default;
 
-function getImageSize(input, callback) {
-  if (typeof callback === 'function') {
-    try {
-      callback(null, imageSize(input));
-    } catch (error) {
-      callback(error);
-    }
-    return;
+function toUint8Array(input) {
+  if (typeof input === 'string') {
+    return new Uint8Array(fs.readFileSync(input));
   }
-  return imageSize(input);
+  if (Buffer.isBuffer(input)) {
+    return new Uint8Array(input.buffer, input.byteOffset, input.byteLength);
+  }
+  if (input instanceof Uint8Array) {
+    return input;
+  }
+  throw new TypeError('image-size expected a Buffer, Uint8Array, or file path');
+}
+
+function getImageSize(input, callback) {
+  try {
+    const result = imageSizeFn(toUint8Array(input));
+    if (typeof callback === 'function') {
+      callback(null, result);
+      return;
+    }
+    return result;
+  } catch (error) {
+    if (typeof callback === 'function') {
+      callback(error);
+      return;
+    }
+    throw error;
+  }
 }
 
 module.exports = getImageSize;
-module.exports.imageSize = imageSize;
+module.exports.imageSize = imageSizeFn;
 module.exports.default = getImageSize;
 if (loaded && typeof loaded === 'object') {
   if (loaded.types) {
